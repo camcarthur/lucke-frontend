c79b9bb10e42efb3fb10b66189945ecd22d00286
Colin McArthur <camcarthur@mavs.coloradomesa.edu>
2025-04-22 09:12:06 -0600

    Redeploy with EB link

--- src/api.js
+++ src/api.js
@@ -1,13 +1,18 @@
 // src/api.js
 
-const API = process.env.REACT_APP_API_URL?.replace(/\/$/, '');
+const API =
+  import.meta.env.VITE_API_URL?.replace(/\/$/, '') ||
+  'https://lucke.eba-idu3y2uj.us-east-2.elasticbeanstalk.com';
 
 export const apiFetch = async (endpoint, options = {}) => {
   const url = `${API}${endpoint.startsWith('/') ? endpoint : `/${endpoint}`}`;
   console.log('Fetching URL:', url);
   return fetch(url, {
     credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+      ...options.headers,
+    },
     ...options,
   });
 };
-
