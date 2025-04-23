6230e344a30712d50ed0e2e7290636a420310bd7
Colin McArthur <camcarthur@mavs.coloradomesa.edu>
2025-04-23 17:59:53 -0600

    Get her goin on https

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 // src/api.js
 const API = process.env.REACT_APP_API_URL?.replace(/\/$/, '') ||
-'http://backend-env-1-env.eba-xxgs9bzn.us-east-2.elasticbeanstalk.com';
+'https://backend-env-1-env.eba-xxgs9bzn.us-east-2.elasticbeanstalk.com/';
 console.log('🛠️ API base URL =', API);
 
 export const apiFetch = async (endpoint, options = {}) => {
